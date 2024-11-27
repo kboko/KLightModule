8e42031a007400c9ff7e176762f29addec40080d
ksv <kokokoko>
2024-11-27 14:16:24 +0100

    Add wifi Driver and test code Driver is taken from https://github.com/beechwoods-software/zephyr-cyw43-driver

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+
+
+int main(void)
+{
+    start_wifi();
+}
+
